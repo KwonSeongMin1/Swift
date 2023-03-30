71f1feca64a865a4602eafd7c0fb33a1ad66c2b8
KwonSeongMin1 <smin9011@gmail.com>
2023-03-30 15:55:05 +0900

    good

--- 프로그래머스/Lv.1/예산.playground/Contents.swift
+++ 프로그래머스/Lv.1/예산.playground/Contents.swift
@@ -1,3 +1,21 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+func solution(_ d:[Int], _ budget:Int) -> Int {
+    
+    var count = 0
+    var result = 0
+    for i in d.sorted(){
+       result += i
+        count += 1
+        if result > budget{
+            count -= 1
+            break
+        }
+        else if result == budget{
+            break
+        }
+    }
+    return count
+}
+
+solution([1, 3, 2, 5, 4], 6)
